d0e6c617a601949530615dfa6b3f145115a1a30d
blue <dylancuprewich@gmail.com>
2019-01-04 14:33:21 -0500

    working properly. Webm location saved to file.Will refine.

--- ygyl_scapper.py
+++ ygyl_scapper.py
@@ -12,39 +12,51 @@ WEBM = ".webm"
 
 def main():
     #boards_info()
-    get_webms()
+    get_webms("wsg")
     
     pass
 
-def get_webms():
-    target_boards = [ basc.Board(name) for name in target_boards_names ]
-    print( target_boards )
-
-    all_threads = [ board.get_all_threads() for board in target_boards ] 
-    print( all_threads )
-    print( all_threads[0])
-    target_threads = [ thread for thread in all_threads[0] if search_for_ygyl( thread.topic ) ]
+def get_webms( target_board_name):
+    print( "Board of focus: "+target_board_name )
+    all_threads = basc.Board(target_board_name).get_all_threads() 
+    target_threads = [ thread for thread in all_threads if search_for_ygyl( thread.topic ) ]
     print( "Threads containing ygyl:",target_threads )
     if not target_threads: # check if there are any threads those topic post contain "ygyl"
-        print( " no ygyl threads ")
+        print( "No current ygyl threads in /"+target_board_name+"/.")
         return
-    files = [ thread.files() for thread in target_threads ]
-    print( files )
-    ffiles = [ item for sublist in files for item in sublist ]
-    print("_____________")
-    print( ffiles )
-    webms = [ file_name for file_name in ffiles if WEBM in file_name]
-    webms_location = open("webms.txt", "w")
-    for webm_name in webms:
-        webms_location.write(webm_name+"\n")
+    all_target_posts = []
+    for thread in target_threads:
+        thread_files = thread.all_posts
+        all_target_posts+=thread_files
+    print( all_target_posts[0] )
+    posts_files = [ post.file for post in all_target_posts if post.has_file]
+    print( posts_files)
 
-    pass
+    files_dictionary = file_dict( posts_files )
+    print( files_dictionary )
+    print( files_dictionary.keys() )
+
+    webms_location = open("webms.txt", "w")
+    other_files_location = open("others.txt", "w")
+    for post_file in posts_files:
+        if post_file.file_extension == WEBM:
+            webms_location.write( post_file.file_url +"\n")
+
+    # create dictionary of file extension, list of files with that extension pairs
+
+def file_dict( file_set ):
+    f_d = {}
+    for f in file_set:
+        if f.file_extension not in f_d.keys(): # file ext not contained
+            f_d[f.file_extension] = [f.file_url]
+        else: # file ext contained in f_d
+            f_d[f.file_extension] += [f.file_url]
+    return f_d
 
 def search_for_ygyl( op_post ):
     subject = op_post.subject if op_post.subject else ""
     body_text = op_post.text_comment if op_post.text_comment else ""
     op_text = subject+body_text
-    print( op_text )
     return any( ygyl_check in op_text for ygyl_check in YGYL)
 
 """
