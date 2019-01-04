4ae9152306d40ebdd6559661b4de3ac5d8a2999e
blue <dylancuprewich@gmail.com>
2019-01-04 17:49:37 -0500

    Downloading now working?

--- README.md
+++ README.md
@@ -3,9 +3,13 @@ A 4chan scapper, using basc-py4chan, that gathers webms from ygyl threads.
 
 ## Current features:
     - allows user to get a file containing links to webms in ygyl threads on /wsg/
+    - allows user to get non-webm links in ygyl threads on /wsg/
 
-## TODO:
-    - allow user to select what boards to choose from
-    - allow user to select what file type to save
-    - allow user to select what keywords to search for
+## Possible additions:
+    - allow user to select what board(s) to choose from
+    - allow user to select what file type(s) to save
+    - allow user to select what keyword(s) to search for
 
+## Required to run:
+    - basc-py4chan module. https://github.com/bibanon/BASC-py4chan
+    - only tested on 3.6.7 . Will probably work on most 3.x. Let me know if any problems occur
--- ygyl_scapper.py
+++ ygyl_scapper.py
@@ -4,13 +4,18 @@ A scrapper for ygyl threads on /gif/ and /wsg/ on www.4chan.org .
 
 import basc_py4chan as basc
 import json as js
+from subprocess import call
 
 YGYL = ["ygyl", "YGYL", "Ygyl" ]
 WEBM = ".webm"
 
 def main():
     #boards_info()
+    input("Will look for webms in /gif/ and /wsg/. Press enter to continue.")
     get_webms("wsg")
+    get_webms("gif")
+    
+
     
     pass
 
@@ -18,7 +23,6 @@ def get_webms( target_board_name):
     print( "Board of focus: "+target_board_name )
     all_threads = basc.Board(target_board_name).get_all_threads() 
     target_threads = [ thread for thread in all_threads if search_for_ygyl( thread.topic ) ]
-    print( "Threads containing ygyl:",target_threads )
     if not target_threads: # check if there are any threads those topic post contain "ygyl"
         print( "No current ygyl threads in /"+target_board_name+"/.")
         return
@@ -26,25 +30,71 @@ def get_webms( target_board_name):
     for thread in target_threads:
         thread_files = thread.all_posts
         all_target_posts+=thread_files
-    posts_files = [ post.file for post in all_target_posts if post.has_file]
+    files_from_posts = [ post.file for post in all_target_posts if post.has_file]
 
-    files_dictionary = file_dict( posts_files )
+    files_object_dictionary = f_o_dict( files_from_posts )
+    files_url_dictionary = file_url_dict( files_from_posts )
 
-    webms_location = open("webms.txt", "w")
-    other_files_location = open("others.txt", "w")
-    for post_file in files_dictionary[WEBM]:
-            webms_location.write( post_file +"\n")
+    wf_name = str( input( 
+        "File to save webm links to. Press enter to save to webms.txt: " ))
+    if not wf_name:
+        wf_name = "webms.txt"
+    nwf_name = str( input(
+        "File to save non-webms to. Press enter to save to others.txt:"
+    ))
+    if not nwf_name:
+        nwf_name = "others.txt"
+
+    webms_location = open( wf_name, "a" )
+    other_files_location = open( nwf_name, "a" )
+    
+    print( "Writing webm links to "+wf_name+"...")
+    webms_location.write("/"+target_board_name+"/:\n")
+    for post_file in files_url_dictionary[WEBM]:
+            webms_location.write( "\t"+post_file +"\n")
     webms_location.close()
-    webms = files_dictionary.pop(WEBM) # removes webms from file set
-    for file_ext in files_dictionary.keys():
-        other_files_location.write(file_ext+":\n")
-        for f in files_dictionary[file_ext]:
-            other_files_location.write("\t"+f+"\n")
+    
+    print("Writing non-webm links to "+nwf_name+"...")
+    other_files_location.write("/"+target_board_name+"/:\n")
+    for file_ext in files_url_dictionary.keys():
+        if file_ext != WEBM:
+            other_files_location.write("\t"+file_ext+":\n")
+            for f in files_url_dictionary[file_ext]:
+                other_files_location.write("\t\t"+f+"\n")
     other_files_location.close()
 
-    # create dictionary of file extension, list of files with that extension pairs
+    to_download( target_board_name, files_object_dictionary)
 
-def file_dict( file_set ):
+
+"""
+
+"""
+def to_download( board_name, files_from_posts ):
+    for ext in files_from_posts.keys():
+        to_d = str( input( "Press enter to download ALL "+ext+" files from "+board_name+". Enter anything else to not." ))
+        if not to_d:
+            for f in files_from_posts[ext]:
+                file_location = f.file_url
+                save_location = f.filename_original
+                call(["curl", file_location, "--output", save_location])
+
+
+"""
+
+"""
+def f_o_dict( file_set ):
+    fo_dict = {}
+    for f in file_set:
+        if f.file_extension not in fo_dict.keys(): # file ext not contained
+            fo_dict[f.file_extension] = [f]
+        else: # file ext contained in f_d
+            fo_dict[f.file_extension] += [f]
+    return fo_dict
+
+"""
+
+"""
+def file_url_dict( file_set ):
     f_d = {}
     for f in file_set:
         if f.file_extension not in f_d.keys(): # file ext not contained
@@ -53,45 +103,13 @@ def file_dict( file_set ):
             f_d[f.file_extension] += [f.file_url]
     return f_d
 
+"""
+
+"""
 def search_for_ygyl( op_post ):
     subject = op_post.subject if op_post.subject else ""
     body_text = op_post.text_comment if op_post.text_comment else ""
     op_text = subject+body_text
     return any( ygyl_check in op_text for ygyl_check in YGYL)
-
-"""
-Returns information related to a set of boards.
-I plan on expanding on board analysis later, webms are of focus now.
-"""
-def boards_info():
-    global b_info 
-    b_info = 0 # board info
-
-    f = open('boards.json')
-    b_info = js.load( f )
-    print( b_info )
-    print( b_info.keys() )
-
-    # empty list. Will contain exclude keys and their value
-    ex_info = [] 
-    
-    # keys to pop. Information not relevant to my purposes
-    ex_keys = ['troll_flags']
-    ex_info = pop_ex( ex_keys )
-
-    print( ex_info )
-    print( b_info.keys())
-    b_info = b_info['boards']
-    for board in b_info:
-        print(board) # print each board information out seperately
-
-    pass
-
-def pop_ex( ex_keys ):
-    global b_info
-    ex_info = []
-    for key in ex_keys:
-        ex_info.append( { key : b_info.pop(key) } )
-    return ex_info
 
 main()
